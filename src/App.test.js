b32a468be0c4ccb1da45252849fb23a22a0e8b5c
devopseasylearning <info@devopseasylearning.com>
2023-10-03 18:27:26 -0500

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div'):
+  ReactDOM.render(App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
