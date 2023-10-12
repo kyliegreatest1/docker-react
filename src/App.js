0cee477e546b2a914676141c0de1f58825e2ca0c
kyliegreatest1 <99826125+kyliegreatest1@users.noreply.github.com>
2023-10-12 18:40:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was Changed!
+          I was Changed to Feature Branch
         </p>
         <a
           className="App-link"
